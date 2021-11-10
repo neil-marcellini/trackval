0cb162ec1dc36864625d3eaf2fe3f6ca6bae2388
neil-marcellini <neil.marcellini@gmail.com>
2021-11-10 11:06:27 -0800

    access token silent refresh

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // const baseURL = 'https://damp-castle-92074.herokuapp.com'
 const baseURL = 'http://localhost:8888'
 let accessToken;
@@ -10,14 +9,88 @@ class App {
     if (params.has('code')) {
       this.handleLogin(params.get('code'));
       return
-    } 
+    }
     // see if the accessToken is set
-    if(accessToken) {
+    if (accessToken) {
       this.showHome();
       return
     }
     // if not try the refresh token
     this.refreshAccessToken();
+  }
+
+  /**
+   * Log the user in given the authorization code. 
+   * @param {string} code The YNAB authorization code.
+   */
+  handleLogin(code) {
+    // get the auth token
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      const authData = JSON.parse(xhr.responseText);
+      this.setAccessToken(authData);
+      // set url
+      window.history.replaceState({}, document.title, '/');
+      this.showHome();
+    }
+    xhr.open('GET', `/oauth?code=${code}`);
+    xhr.send();
+  }
+
+  /**
+   * Request a new access token using the
+   * refresh token set as a http only cookie.
+   */
+  refreshAccessToken() {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      if (xhr.status === 401) {
+        // refresh failed, show login
+        this.showLogin();
+        return;
+      }
+      let accessTokenWasNotSet = accessToken === undefined
+      const refreshData = JSON.parse(xhr.responseText);
+      this.setAccessToken(refreshData);
+      // if the accessToken was just set, show the home page
+      if(accessTokenWasNotSet) {
+        this.showHome();
+      }
+    }
+    xhr.open('GET', '/refresh');
+    xhr.send();
+  }
+
+  /**
+   * @typedef {Object} TokenResponse
+   * @property {string} access_token The access token for the YNAB api.
+   * @property {number} expires_in The number of seconds until the access token expires.
+   */
+
+  /**
+   * Set the access token and expire time
+   * and silently refresh the access token before it expires.
+   * @param {TokenResponse} tokenData
+   */
+  setAccessToken(tokenData) {
+    // update the token and expire time
+    accessToken = tokenData.access_token;
+    accessExpires = tokenData.expires_in;
+    this.silentlyRefreshToken();
+  }
+
+  /**
+   * Before the current access token expires
+   * refresh the access token.
+   */
+  silentlyRefreshToken() {
+    // The number of seconds before expiration to refresh.
+    const secondsEarly = 60;
+    const refreshSeconds = accessExpires - secondsEarly;
+    const refreshMilliseconds = refreshSeconds * 1000;
+    setTimeout(() => {
+      this.refreshAccessToken();
+    }, refreshMilliseconds)
   }
 
   /**
@@ -28,7 +101,7 @@ class App {
    * Clear the contents of the app and show the new content. 
    * @param {HTMLElement} newContent The new app content.
    */
-   update(newContent) {
+  update(newContent) {
     // clear content
     this.ref.innerHTML = "";
     // add new content
@@ -44,47 +117,6 @@ class App {
   }
 
   /**
-   * Request a new access token using the
-   * refresh token set as a http only cookie.
-   */
-  refreshAccessToken() {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = () => {
-      if (xhr.status === 401) {
-        // refresh failed, show login
-        this.showLogin();
-        return;
-      }
-      const refreshData = JSON.parse(xhr.responseText);
-      // update the token and expire time
-      accessToken = refreshData.access_token;
-      accessExpires = refreshData.expires_in;
-      this.showHome();
-    }
-    xhr.open('GET', '/refresh');
-    xhr.send();
-  }
-
-  /**
-   * Log the user in given the authorization code. 
-   * @param {string} code The YNAB authorization code.
-   */
-  handleLogin(code) {
-    // get the auth token
-    const xhr = new XMLHttpRequest();
-    xhr.onload = () => {
-      const authData = JSON.parse(xhr.responseText);
-      accessToken = authData.access_token;
-      accessExpires = authData.expires_in;
-      // set url
-      window.history.replaceState({}, document.title, '/');
-      this.showHome();
-    }
-    xhr.open('GET', `/oauth?code=${code}`);
-    xhr.send();
-  }
-
-  /**
    * Shows the home page.
    */
   showHome() {
@@ -92,5 +124,6 @@ class App {
     home.innerText = 'Home';
     this.update(home);
   }
+
 }
 const app = new App();
